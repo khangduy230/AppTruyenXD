35fe35b4702a0fa551608c75e442ad5e6cc84281
tndghi <haike.tn147@gmail.com>
2026-05-12 20:10:22 +0700

    thêm giao diện darkmode

---
+++ app/src/main/java/com/example/truyentranhthanhxuan/MyApplication.java
@@ -0,0 +1,26 @@
+package com.example.truyentranhthanhxuan;
+
+import android.app.Application;
+import android.content.SharedPreferences;
+import androidx.appcompat.app.AppCompatDelegate;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //khởi tạo SharedPreferences
+        SharedPreferences sharedPreferences = getSharedPreferences("AppSettingPrefs", MODE_PRIVATE);
+
+        //lấy trạng thái hiện tại (mặc định là false - Light Mode)
+        boolean isNightMode = sharedPreferences.getBoolean("NightMode", false);
+
+        //set chế độ hiện tại (Light/Dark)
+        if (isNightMode) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+    }
+}
