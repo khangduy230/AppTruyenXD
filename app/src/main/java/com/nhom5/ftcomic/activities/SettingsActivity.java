9c819a42cad4129656c82937e701ffd3ad2bece5
The Duyet <duyetn64@gmail.com>
2026-05-21 22:58:20 +0700

    Thêm dialog

--- app/src/main/java/com/nhom5/ftcomic/activities/SettingsActivity.java
+++ app/src/main/java/com/nhom5/ftcomic/activities/SettingsActivity.java
@@ -1,8 +1,23 @@
 package com.nhom5.ftcomic.activities;
 
+import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
@@ -10,43 +25,45 @@ import com.nhom5.ftcomic.R;
 import com.google.android.material.appbar.MaterialToolbar;
 
 public class SettingsActivity extends AppCompatActivity {
-    //khai báo các biến cần thiết
+    //khai báo các biến
     private Switch switchDarkMode;
-    //SharedPreferences dùng để lưu lại các cài đặt khi tắt ứng dụng
-    //và mở lại thì các thiết lập không mất đi.
     private SharedPreferences sharedPreferences;
-
+    private LinearLayout layouttansuat;
+    private TextView edttanxuat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        //ánh xạ các biến
-        switchDarkMode = findViewById(R.id.switchDarkMode);
 
+        // Ánh xạ các biến
+        switchDarkMode = findViewById(R.id.switchDarkMode);
+        layouttansuat = findViewById(R.id.layouttansuat);
+        edttanxuat = findViewById(R.id.edttanxuat);
         //khởi tạo SharedPreferences để lưu cấu hình
         sharedPreferences = getSharedPreferences("AppSettingPrefs", MODE_PRIVATE);
 
-        //lấy trạng thái hiện tại (mặc định là false - Light Mode)
-        boolean isNightMode = sharedPreferences.getBoolean("NightMode", false);
 
-        //set trạng thái cho Switch
+        boolean isNightMode = sharedPreferences.getBoolean("NightMode", false);
         switchDarkMode.setChecked(isNightMode);
 
-        //xử lý sự kiện khi gạt Switch
+        layouttansuat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFeedbackDialog(Gravity.CENTER);
+            }
+        });
+
         switchDarkMode.setOnCheckedChangeListener((buttonView, isChecked) -> {
             SharedPreferences.Editor editor = sharedPreferences.edit();
-
             if (isChecked) {
-                //bật Dark Mode
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
                 editor.putBoolean("NightMode", true);
             } else {
-                //tắt Dark Mode (về Light Mode)
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
                 editor.putBoolean("NightMode", false);
             }
-            editor.apply(); // lưu lại thiết lập
+            editor.apply();
         });
 
         //tìm view theo id
@@ -71,4 +88,56 @@ public class SettingsActivity extends AppCompatActivity {
             // Hoặc dùng một lệnh cực kỳ ngắn gọn và phổ biến để đóng Activity hiện tại:
             // finish();
         });
-    }}
+    }
+
+    private void openFeedbackDialog(int gravity) {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.layout_dialog_tanxuat);
+
+        Window window = dialog.getWindow();
+        if (window == null) return;
+
+        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        WindowManager.LayoutParams windowAttributes = window.getAttributes();
+        windowAttributes.gravity = gravity;
+        window.setAttributes(windowAttributes);
+
+        if (Gravity.CENTER == gravity) {
+            //Khi click ra bên ngoài thì tắt
+            dialog.setCancelable(true);
+        }else
+        {
+            dialog.setCancelable(false);
+        }
+        //ánh xạ id các thành phần trong dialog
+        RadioGroup grouptansuat = dialog.findViewById(R.id.grouptansuat);
+        Button btnback = dialog.findViewById(R.id.btnback);
+        Button btnconfirm = dialog.findViewById(R.id.btnconfirm);
+        //xử lý sự kiện khi click vào btnback
+        btnback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        //Xử lí sự kiện khi click vào btnconfirm
+        btnconfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selectedId = grouptansuat.getCheckedRadioButtonId();
+                RadioButton radioButton = dialog.findViewById(selectedId);
+                String selectedText = radioButton.getText().toString();
+                if (edttanxuat != null) {
+                    edttanxuat.setText(selectedText);
+                }
+                dialog.dismiss();
+
+            }
+        });
+
+        dialog.show();
+    }
+}
