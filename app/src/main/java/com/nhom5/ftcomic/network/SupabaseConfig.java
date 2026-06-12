e08c18b2c0ad0c688efb560afa2a2fbdbeea0fc8
Hoàng Nguyên Duy <hoangduy442k5@gmail.com>
2026-06-12 20:53:32 +0700

    test supabase

--- app/src/main/java/com/nhom5/ftcomic/network/SupabaseConfig.java
+++ app/src/main/java/com/nhom5/ftcomic/network/SupabaseConfig.java
@@ -2,11 +2,11 @@ package com.nhom5.ftcomic.network;
 
 public class SupabaseConfig {
 
-    public static final String PROJECT_URL = "https://enkliqnftrprfyszkyov.supabase.co/";
+    public static final String PROJECT_URL = "https://pcdjenihcwddqzbhwusx.supabase.co/";
 
     public static final String BASE_URL = PROJECT_URL + "rest/v1/";
 
     public static final String AUTH_BASE_URL = PROJECT_URL + "auth/v1/";
 
-    public static final String API_KEY = "sb_publishable_AmtjAUXr15c4VmXZfl-UEg_Njdqm6Zq";
+    public static final String API_KEY = "sb_publishable_YhQu9gIg4CgJifgm4xCSXQ_PzgJImF8";
 }
